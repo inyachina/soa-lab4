5d283d637f15c936b6b0be707aa11acbd3c6ac5e
inyachina <dianainyachina@gmail.com>
2023-01-21 04:26:18 +0300

    create xsd schema + refactor controller to jax-ws endpoint

--- soa-second-service/src/main/java/com/soa/lab2/contoller/LabsController.java
+++ soa-second-service/src/main/java/com/soa/lab2/contoller/LabsController.java
@@ -1,32 +1,38 @@
 package com.soa.lab2.contoller;
 
 import com.soa.lab2.model.Lab;
-import com.soa.lab2.service.LabService;
 import com.soa.lab2.service.LabServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import soa.lab4.DecreaseLabDifficultyRequest;
+import soa.lab4.GetHardcoreLabsRequest;
 
 import java.util.List;
 
 @Slf4j
-@RestController
-@RequestMapping("bars/api/v1")
+@Endpoint
 @AllArgsConstructor
 public class LabsController {
+    private static final String NAMESPACE_URI = "http://soa/lab4";
     private final LabServiceImpl labService;
 
-    @GetMapping("/disciplines/{discipline-id}/get-hardcore")
-    private ResponseEntity getHardcore(@PathVariable(name = "discipline-id") Integer disciplineId) {
-        List<Lab> labs = this.labService.getHardcoreLabs(disciplineId);
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getHardcoreLabsRequest")
+    @ResponsePayload
+    private ResponseEntity getHardcore(@RequestPayload GetHardcoreLabsRequest request) {
+        List<Lab> labs = this.labService.getHardcoreLabs(request.getDisciplineId());
         if (labs.isEmpty()) return ResponseEntity.noContent().build();
 
         return ResponseEntity.ok().body(labs);
     }
 
-    @PostMapping("/labwork/{labwork-id}/difficulty/decrease/{steps-count}")
-    private ResponseEntity decreaseLabDifficulty(@PathVariable("labwork-id") Integer labId, @PathVariable("steps-count") Integer stepsCount) {
-        return ResponseEntity.ok().body(this.labService.decreaseLabDifficulty(labId, stepsCount));
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "decreaseLabDifficultyRequest")
+    @ResponsePayload
+    private ResponseEntity decreaseLabDifficulty(@RequestPayload DecreaseLabDifficultyRequest request) {
+        return ResponseEntity.ok().body(this.labService.decreaseLabDifficulty(request.getDisciplineId(), request.getDisciplineId()));
     }
 }
